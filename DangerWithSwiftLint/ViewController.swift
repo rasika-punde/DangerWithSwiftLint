56b7a81dda1651594916c2ab6080d78e6a710880
Rasika Punde <rapunde@deloitte.com>
2019-10-16 17:25:04 +0530

    Added new func

---
+++ DangerWithSwiftLint/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  DangerWithSwiftLint
+//
+//  Created by Punde, Rasika Nanasaheb on 16/10/19.
+//  Copyright © 2019 Punde, Rasika Nanasaheb (US - Mumbai). All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    let hi = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func asdfghjklpoiuytrewqzxcvbnmjasghdgkjasghdjhsakjdgjksagdjgas() {
+        
+    }
+
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pods/Target Support Files/Pods-DangerWithSwiftLint/Pods-DangerWithSwiftLint-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DangerWithSwiftLint : NSObject
+@end
+@implementation PodsDummy_Pods_DangerWithSwiftLint
+@end
